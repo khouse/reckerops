6c3d02ee86241ae7024c9a294fc1a8e73d699afd
Alex Recker <alex@reckerfamily.com>
2017-01-30 11:13:30 -0600

    Adds CORS to lambda

--- blog.tf
+++ blog.tf
@@ -115,6 +115,11 @@ resource "aws_api_gateway_method_response" "randomimage200" {
   resource_id = "${aws_api_gateway_resource.randomimage.id}"
   http_method = "${aws_api_gateway_method.randomimageget.http_method}"
   status_code = "200"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = true,
+    "method.response.header.Access-Control-Allow-Methods" = true,
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
 }
 
 
@@ -123,6 +128,11 @@ resource "aws_api_gateway_integration_response" "randomimage" {
   resource_id = "${aws_api_gateway_resource.randomimage.id}"
   http_method = "${aws_api_gateway_method.randomimageget.http_method}"
   status_code = "${aws_api_gateway_method_response.randomimage200.status_code}"
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'",
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
 }
 
 resource "aws_api_gateway_deployment" "blog" {
