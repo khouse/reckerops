b06f6757ded54412e444b8b34e070af696fa452a
Alex Recker <alex@reckerfamily.com>
2017-02-18 15:29:03 -0600

    Fixes

--- cloudformation/templates/app.rb
+++ cloudformation/templates/app.rb
@@ -1,4 +1,8 @@
-SparkleFormation.new(:vpc) do
+SparkleFormation.new(:app) do
   description 'A simple public service'
   dynamic!(:network)
+  dynamic!(:security, ref!(:network_vpc),
+           :ssh_anywhere => true,
+           :http_anywhere => true,
+           :https_anywhere => true)
 end
--- cloudformation/templates/packer.rb
+++ cloudformation/templates/packer.rb
@@ -1,20 +1,7 @@
-SparkleFormation.new(:vpc) do
+SparkleFormation.new(:packer) do
   description 'A packer-ready network stack'
-
   dynamic!(:network)
-
-  resources.security_group do
-    Type 'AWS::EC2::SecurityGroup'
-    Properties do
-      GroupDescription 'Allow SSH to packer builders'
-      VpcId ref!(:network_vpc)
-      Tags tags!({:Name => stack_name!})
-      SecurityGroupIngress do
-        IpProtocol 'tcp'
-        FromPort '22'
-        ToPort '22'
-        CidrIp '0.0.0.0/0'
-      end
-    end
-  end
+  dynamic!(:security, ref!(:network_vpc),
+           :description => 'Allow SSH to packer builders',
+           :ssh_anywhere => true)
 end
