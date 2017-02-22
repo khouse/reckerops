17a7b0e38dea497ef2598e12f6a558e1d467aa4a
Alex Recker <alex@reckerfamily.com>
2017-02-21 18:21:48 -0600

    Starting over

--- cfn/templates/app.rb
+++
@@ -1,8 +0,0 @@
-SparkleFormation.new(:app) do
-  description 'A simple public service'
-  dynamic!(:network)
-  dynamic!(:security, ref!(:network_vpc),
-           :ssh_anywhere => true,
-           :http_anywhere => true,
-           :https_anywhere => true)
-end
--- cfn/templates/packer.rb
+++
@@ -1,7 +0,0 @@
-SparkleFormation.new(:packer) do
-  description 'A packer-ready network stack'
-  dynamic!(:network)
-  dynamic!(:security, ref!(:network_vpc),
-           :description => 'Allow SSH to packer builders',
-           :ssh_anywhere => true)
-end
