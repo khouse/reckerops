8d5cbb7d3a67dca0fc1204ddaae1afef76222d64
Alex Recker <alex@reckerfamily.com>
2017-03-08 01:53:54 -0600

    Key stuff

--- test/integration/default/serverspec/user_spec.rb
+++ test/integration/default/serverspec/user_spec.rb
@@ -2,8 +2,16 @@ require 'serverspec'
 
 set :backend, :exec
 
+SSH_KEYS= [
+  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD/J98LsMpGwuNA/U3jpqOB0YcD8TNO5CSYZmFm7liDWT2Ndw6uOt3wEQx7LcYh1aDeI6XsHkqNSUoMbgVmFQ10KzEWTd/F+JIE/2aQ5uowJ5/DynobXEgAt/c7FqlnCWWK4u4MbAc5oFLn2sc/eeg1FbQP8EnYJ+8Qi0T/ny9gSLHun8oSAI2Ae74iKnPYyEqozZZf46LToR30W3C/6w0kN6k7H9M3IgrZxWuvRZTsQIvl8meo0YxItTR7HDiqSMav5ZXah6mT7OHhCTN0LIReOPsQu2j4Dc96cfWaIfoSTwQtsx/Teoc/BjhNfawj+YwjRk6li36QUw8xjtGauCLivqrDiB2omFrqY3X15JgMQQkSxiRO4TMFjJSKX52vh9YR5P62uuYyZMqzBvn7QfWG4qQWkPkivX3f3TpIGwmF68Adyaz1+bkFnFRcgytGM960w7RVUp2s98ndoqeRK2av6UJvutqXvPyCbKJvHKUIjQ3UivwwienSTuTXDUYT3hjruz1umwMAcRqqadHaNOBViPT7dvg5t+ruCeJedjtdhXRvsiKiFucq8fxzev46jMD/zCq4hlQUV5Uk8cYYT8dius3TojUddgaEV3by29B5h4/6sVjC/MVCIHu4r+qup2a4X9PL+lrVDQyjD0tdHfBI86FBwUV/EuiFxzrvaCGJQw== alex@lappy-486',
+  'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDbtSqOouv5nPM3Zk+NGmlla1wuSP/OtXHDTl2svxT/BJMZburyTQwdloQL07Koe3+786Saf7pWIrjl4zVtg+rIsnxYaDsGxh2FyGvYu5qHBiQorD8hho+8VysNk21KrQXlVc22cgKiOPIjUjRg6LsWxC/1LKoo1xlrt+SOcJ11CaRHXx7dpJYvjact/XxEonwVtwww3K+9Ao9ZrOHPWHwMkQ5DMTXCm7KO/UiVMS83pTpRkYGJZ6h71dNMOSEXwSJ3LUPRa1/BjZLysl1kaSni8F6zaAO39rSYDwoYK5TTf4RVyU0W+iOr2Waa7H9EsEs4G8Ep3BdQbUJLb8T96dhgKXyRVjL9lfym9BKAubARNU4gi9V73tSvj+ZU1mMRtJHMPfS+3+SYlDN2Pnfa6G2Au/eki3BVfZquZGswgtSPxorqXw1Sf4FOe6cSctNlQKH4OLywsOAzB/RLLlYWxAibNsaRpCHT8wyeq16VPoprpZJILDHNBOmjccY/fF0B1iYrpHczYnkdk827IKBw+Axxx3v5cUyDLo7HqsvtOZEeQBtlIkxShJJlIFEWziFhhefDq5Gzv7u2mXcvGUThWHrI7gVC84i1mgVYmyEcYUaXyaCXI9Ask3+efI2sEbPO5VCkg/2Km1gF12rC5rlOtSJMI4eCLFXKodkbAToFKHLjbw== alex@funkytown'
+].freeze
+
 describe user('alex') do
   it { should belong_to_group('sudo') }
   it { should have_home_directory('/home/alex') }
   it { should have_login_shell('/bin/bash') }
+  SSH_KEYS.each do |key|
+    it { should have_authorized_key key }
+  end
 end
