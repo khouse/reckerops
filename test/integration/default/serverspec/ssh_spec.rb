fc5645f9c2bf5d1c0f267de8ab0aa0036ff4ef59
Alex Recker <alex@reckerfamily.com>
2017-03-07 19:49:18 -0600

    Adds tests

---
+++ test/integration/default/serverspec/ssh_spec.rb
@@ -0,0 +1,15 @@
+require 'serverspec'
+
+set :backend, :exec
+
+describe package('openssh-server') do
+  it { should be_installed }
+end
+
+describe service('ssh') do
+  it { should be_running }
+end
+
+describe port(22) do
+  it { should be_listening }
+end
