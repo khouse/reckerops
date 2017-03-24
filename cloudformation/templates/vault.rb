f53e929ed7f1075c2e092a46d0defa324aac041d
Alex Recker <alex@reckerfamily.com>
2017-03-23 20:07:10 -0500

    Vault

---
+++ cloudformation/templates/vault.rb
@@ -0,0 +1,54 @@
+SparkleFormation.new(:vault) do
+  policy = {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Effect: "Allow",
+        Action: [
+          "codecommit:BatchGetRepositories",
+          "codecommit:Get*",
+          "codecommit:List*",
+          "codecommit:Put*",
+          "codecommit:Test*",
+          "codecommit:Update*",
+          "codecommit:GitPull",
+          "codecommit:GitPush"
+        ],
+        Resource: attr!(:repository, :arn)
+      }
+    ]
+  }
+  resources.user do
+    Type 'AWS::IAM::User'
+    Properties do
+      UserName join!(stack_name!, '-', region!)
+      Policies [
+        PolicyName: 'VaultRepoPolicy',
+        PolicyDocument: policy
+      ]
+    end
+  end
+  resources.topic do
+    Type 'AWS::SNS::Topic'
+    Properties do
+      DisplayName stack_name!
+      TopicName vault
+    end
+  end
+  resources.repository do
+    Type 'AWS::CodeCommit::Repository'
+    Properties do
+      RepositoryName 'vault'
+      Triggers [
+        Name: 'VaultNotifyChange',
+        DestinationArn: ref!(:topic),
+        Events: ['all'],
+        Branches: ['Master']
+      ]
+    end
+  end
+  outputs.url do
+    Description 'Clone URL'
+    Value attr!(:repository, :clone_url_ssh)
+  end
+end
