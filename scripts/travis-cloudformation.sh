91ec74e196e6a4560a4e8a8451506a34648b5a36
Alex Recker <alex@reckerfamily.com>
2017-03-26 23:57:44 -0500

    Fixes travis

--- scripts/travis-cloudformation.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd ../cloudformation/
-bundle exec stack_master validate && bundle exec stack_master apply --yes
--- scripts/travis-kitchen.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-cd ..
-bundle exec kitchen converge $1
-success=$?
-bundle exec kitchen destroy $1
-exit $success
