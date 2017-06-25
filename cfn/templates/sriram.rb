a162356f0f963564cacb445b9c4463506fc477a0
Alex Recker <alex@reckerfamily.com>
2017-06-25 12:32:07 -0500

    Redid cloudformation crap

--- cfn/templates/sriram.rb
+++
@@ -1,4 +0,0 @@
-SparkleFormation.new(:sriram) do
-  dynamic!(:r_bucket_web, :scrabble_bucket, domain: 'scrabble.sarahrecker.com')
-  outputs.scrabble_url { Value attr!(:scrabble_bucket, :WebsiteURL) }
-end
